6e04c5f84059d933b847dae14ba96f051638c156
laura-williams-1 <laurawilliams@pursuit.org>
2023-07-22 16:59:13 -0400

    initalizing useState in Form

--- src/Components/JoinUs.js
+++ src/Components/JoinUs.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const JoinUs = () => {
+  const [formData, setFormData] = useState({
+    fullName: "",
+    email: "",
+    phoneNumber: "",
+    address: "",
+    dateOfBirth: "",
+    availability: "",
+    skills: "",
+    interests: "",
+    volunteerExperience: "",
+    professionalExperience: "",
+    motivation: "",
+    goals: "",
+    references: "",
+    emergencyContact: "",
+    backgroundCheckConsent: false,
+  });
   return (
     <div>
       <h1>This is to Join Us Form</h1>
     </div>
-  )
-}
+  );
+};
 
-export default JoinUs
+export default JoinUs;
