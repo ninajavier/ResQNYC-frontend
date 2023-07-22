06980040916e32bdcd70f689e4e7b3c4ba4f389a
laura-williams-1 <laurawilliams@pursuit.org>
2023-07-22 17:17:03 -0400

    all handler function created few forum inputs complete

--- src/Components/JoinUs.js
+++ src/Components/JoinUs.js
@@ -18,10 +18,59 @@ const JoinUs = () => {
     emergencyContact: "",
     backgroundCheckConsent: false,
   });
+
+  const textHandler = (e) => {};
+
+  const checkBoxHandler = (e) => {};
+
+  const submitHandler = (e) => {};
   return (
-    <div>
-      <h1>This is to Join Us Form</h1>
-    </div>
+    <form onSubmit={submitHandler}>
+      {/* Your form fields go here */}
+      <input
+        type="text"
+        name="fullName"
+        value={formData.fullName}
+        onChange={textHandler}
+        placeholder="Full Name"
+        required
+      />
+
+      <input
+        type="email"
+        name="email"
+        value={formData.email}
+        onChange={textHandler}
+        placeholder="yourEmail@domain.com"
+        required
+      />
+      <input
+        type="text"
+        name="phoneNumber"
+        value={formData.phoneNumber}
+        onChange={textHandler}
+        placeholder="000-000-000"
+        required
+      />
+      <input
+        type="text"
+        name="phoneNumber"
+        value={formData.phoneNumber}
+        onChange={textHandler}
+        placeholder="000-000-000"
+        required
+      />
+      <input
+        type="checkbox"
+        name="backgroundCheckConsent"
+        checked={formData.backgroundCheckConsent}
+        onChange={checkBoxHandler}
+      />
+      <label htmlFor="backgroundCheckConsent">
+        I consent to a background check.
+      </label>
+      <button type="submit">Submit</button>
+    </form>
   );
 };
 
