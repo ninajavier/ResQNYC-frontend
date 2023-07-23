6c20365b9952361331e353240e07a5efa738d133
ninajavier <ninajavier13@gmail.com>
2023-07-23 00:32:29 -0400

    Working on map modal

--- src/Components/EvacMapModal.js
+++ src/Components/EvacMapModal.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 
-const EvacMapModal = () => {
+const EvacMapModal = ({ data }) => {
   return (
     <div>
-      <h1>Throws Evacuation Data</h1>
+      <div className='evac-sites-map'>
+
+      </div>
       <div>
       <iframe 
       allow="geolocation" src="https://data.cityofnewyork.us/dataset/Hurricane-Inundation-by-Evacuation-Zone-Map-/2234-9r2y/embed?width=800&height=600" title="Evacuation Map" width="800" height="600" style={{border:"0", padding: "0", margin: "0"}}
